94767a0a239d1f48eaecb93a75e1ddf7a7e7b5d0
John Hannagan <weetbix_@hotmail.com>
2021-07-01 21:27:17 +0800

    Use babel for new js features

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,7 @@
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
+import json from '@rollup/plugin-json';
+import babel from '@rollup/plugin-babel';
 
 export default {
     input: 'src/index.js',
@@ -14,5 +16,10 @@ export default {
             mainFields: ['main'],
         }),
         commonjs(),
+        babel({
+            babelHelpers: 'runtime',
+            exclude: 'node_modules/**'
+        }),
+        json(),
     ],
 };
