944e9608a44915e564315a6103164451d851c050
Weetbix <weetbix_@hotmail.com>
2022-09-15 20:30:20 +0800

    Improve the developer commands (#13)

---
+++ src/cli/debug-file.js
@@ -0,0 +1,20 @@
+import {Command} from 'commander';
+import fs from 'fs';
+import {preprocessMarkdown} from '../readability'
+
+const program = new Command();
+program
+  .name('debug file')
+  .description('Show what the program parses form a single file, after stripping the junk away')
+
+program
+  .argument('<file>', 'the filepath to show the stripped input for')
+program.parse();
+
+const [, , filePath] = process.argv;
+
+const markdown = fs.readFileSync(filePath);
+const stripped = preprocessMarkdown(markdown);
+
+console.log('------ stripped file input: ------');
+console.log(stripped);
--- src/cli/report.js
+++ src/cli/report.js
@@ -1,23 +1,35 @@
 import path from 'path';
 import glob from 'glob';
-import {calculateReadability} from './readability';
-import {generateReport} from './report';
-import {reportToComment} from './markdown';
+import {Command} from 'commander';
+import {calculateReadability} from '../readability';
+import {generateReport} from '../report';
+import {reportToComment} from '../markdown';
 
+const program = new Command();
+program
+  .name('report')
+  .description('Show generated report output in console')
+
+program
+  .argument('<oldFolder>', 'old folder to compare to')
+  .argument('<newFolder>', 'new folder to compare to')
+  .option('-g, --glob <pattern>', 'The glob to use when matching files', '**/*.{md,mdx}')
+program.parse();
+
+const GLOB = program.opts().glob
 const [, , oldFolder, newFolder] = process.argv;
 
-const ALL_MD_FILES = '**/*.md';
 const ROOT_DIR = process.cwd();
 
 // Run the readability stats on all MD files in the first folder provided
 process.chdir(path.join(ROOT_DIR, oldFolder));
-const allOldFiles = glob.sync(ALL_MD_FILES);
-const oldReadability = calculateReadability(ALL_MD_FILES);
+const allOldFiles = glob.sync(GLOB);
+const oldReadability = calculateReadability(GLOB);
 
 // Run the readability stats on all MD files in the second folder provided
 process.chdir(path.join(ROOT_DIR, newFolder));
-const allNewFiles = glob.sync(ALL_MD_FILES);
-const newReadability = calculateReadability(ALL_MD_FILES);
+const allNewFiles = glob.sync(GLOB);
+const newReadability = calculateReadability(GLOB);
 
 // We dont have a real PR, so generate modified/added files based
 // on the difference of files in the input folders
