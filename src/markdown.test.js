622f93ab85d443e7bff8c47f8a1ba3e1167d30c4
John Hannagan <weetbix_@hotmail.com>
2023-06-12 20:14:58 +1000

    Update markdown tests

--- src/markdown.test.js
+++ src/markdown.test.js
@@ -1,6 +1,7 @@
 import {reportToComment} from './markdown';
 
 const mockScores = (value) => ({
+    readabilityScore: value,
     fleschReadingEase: value,
     gunningFog: value,
     smogIndex: value,
@@ -46,7 +47,41 @@ describe('reportToComment', () => {
 
         expect(reportToComment({report})).toMatchInlineSnapshot(`
             "
-            Readability after merging this PR:
+            Readability after merging this PR: 1/100 (🟢 +1)
+
+            Path | Readability
+            --- | ---
+            [file-1.md](https://github.com/repo-name/blob/commit-sha/./folder/file-1.md \\"./folder/file-1.md\\") | 10 (🟢 +1)
+            [file-2.md](https://github.com/repo-name/blob/commit-sha/./folder/file-2.md \\"./folder/file-2.md\\") | 5 (🔴 -1)
+            [file-3.md](https://github.com/repo-name/blob/commit-sha/./folder/file-3.md \\"./folder/file-3.md\\") | 15 (🟢 +0)
+            [new-file.md](https://github.com/repo-name/blob/commit-sha/./folder/new-file.md \\"./folder/new-file.md\\") | 20 (-)
+
+
+            <details>
+              <summary>View Detailed Metrics</summary>
+
+            🟢 - Shows an _increase_ in readability
+            🔴 - Shows a _decrease_ in readability
+
+            Path | Readability | FRE | GF | ARI | CLI | DCRS
+            --- | --- | --- | --- | --- | --- | ---
+            [file-1.md](https://github.com/repo-name/blob/commit-sha/./folder/file-1.md \\"./folder/file-1.md\\") | 10 | 10 | 10 | 10 | 10 | 10
+            &nbsp; | 🟢 +1 | 🟢 +1 | 🔴 +1 | 🔴 +1 | 🔴 +1 | 🔴 +1
+            [file-2.md](https://github.com/repo-name/blob/commit-sha/./folder/file-2.md \\"./folder/file-2.md\\") | 5 | 5 | 5 | 5 | 5 | 5
+            &nbsp; | 🔴 -1 | 🔴 -1 | 🟢 -1 | 🟢 -1 | 🟢 -1 | 🟢 -1
+            [file-3.md](https://github.com/repo-name/blob/commit-sha/./folder/file-3.md \\"./folder/file-3.md\\") | 15 | 15 | 15 | 15 | 15 | 15
+            &nbsp; | 🟢 +0 | 🟢 +0 | 🟢 0 | 🟢 0 | 🟢 0 | 🟢 0
+            [new-file.md](https://github.com/repo-name/blob/commit-sha/./folder/new-file.md \\"./folder/new-file.md\\") | 20 | 20 | 20 | 20 | 20 | 20
+            &nbsp; | - | - | - | - | - | -
+
+
+            Averages:
+
+            &nbsp; | Readability | FRE | GF | ARI | CLI | DCRS
+            --- | --- | --- | --- | --- | --- | ---
+            Average | 1 | 1 | 1 | 1 | 1 | 1
+            &nbsp; | 🟢 +1 | 🟢 +1 | 🔴 +1 | 🔴 +1 | 🔴 +1 | 🔴 +1
+
 
             <details>
               <summary>View Metric Targets</summary>
@@ -55,36 +90,13 @@ describe('reportToComment', () => {
             --- | --- | ---
             Flesch Reading Ease | 100 (very easy read) to 0 (extremely difficult read) | 60
             Gunning Fog | 6 (very easy read) to 17 (extremely difficult read) | 8 or less
-            SMOG Index | 6 (very easy read) to 14 (extremely difficult read) | 8 or less
             Auto. Read. Index | 6 (very easy read) to 14 (extremely difficult read) | 8 or less
             Coleman Liau Index | 6 (very easy read) to 17 (extremely difficult read) | 8 or less
-            Linsear Write | 0 (very easy read) to 11 (extremely difficult read) | 8 or less
             Dale-Chall Readability | 4.9 (very easy read) to 9.9 (extremely difficult read) | 6.9 or less
 
             </details>
 
-            🟢 - Shows an _increase_ in readability
-            🔴 - Shows a _decrease_ in readability
-
-            Path | FRE | GF | SMOG | ARI | CLI | LWF | DCRS
-            --- | --- | --- | --- | --- | --- | --- | ---
-            [file-1.md](https://github.com/repo-name/blob/commit-sha/./folder/file-1.md \\"./folder/file-1.md\\") | 10 | 10 | 10 | 10 | 10 | 10 | 10
-            &nbsp; | 🟢 +1 | 🔴 +1 | 🔴 +1 | 🔴 +1 | 🔴 +1 | 🔴 +1 | 🔴 +1
-            [file-2.md](https://github.com/repo-name/blob/commit-sha/./folder/file-2.md \\"./folder/file-2.md\\") | 5 | 5 | 5 | 5 | 5 | 5 | 5
-            &nbsp; | 🔴 -1 | 🟢 -1 | 🟢 -1 | 🟢 -1 | 🟢 -1 | 🟢 -1 | 🟢 -1
-            [file-3.md](https://github.com/repo-name/blob/commit-sha/./folder/file-3.md \\"./folder/file-3.md\\") | 15 | 15 | 15 | 15 | 15 | 15 | 15
-            &nbsp; | 🟢 +0 | 🟢 0 | 🟢 0 | 🟢 0 | 🟢 0 | 🟢 0 | 🟢 0
-            [new-file.md](https://github.com/repo-name/blob/commit-sha/./folder/new-file.md \\"./folder/new-file.md\\") | 20 | 20 | 20 | 20 | 20 | 20 | 20
-            &nbsp; | - | - | - | - | - | - | -
-
-
-            Overall average:
-
-            &nbsp; | FRE | GF | SMOG | ARI | CLI | LWF | DCRS
-            --- | --- | --- | --- | --- | --- | --- | ---
-            Average | 1 | 1 | 1 | 1 | 1 | 1 | 1
-            &nbsp; | 🟢 +1 | 🔴 +1 | 🔴 +1 | 🔴 +1 | 🔴 +1 | 🔴 +1 | 🔴 +1
-
+            </details>
             "
         `);
     });
