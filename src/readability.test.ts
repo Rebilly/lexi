24368f6bf42ae5b11af3cc3f98682ad9175c84b3
Chris <85164331+noon-dawg@users.noreply.github.com>
2025-05-22 11:49:32 +0100

    Fix indentation and format

--- src/readability.test.ts
+++ src/readability.test.ts
@@ -207,11 +207,10 @@ This content comes after the table
              1. This is a test document. \
                 Several lines new lines are created using the backslash character.`,
         );
-        expect(stripped).toMatchInlineSnapshot(`"
-             1. This is a test for use of the backslash character in Markdown.
-                In this process, several lines are created using it.
-             1. This is a test document.
-                Several lines new lines are created using the backslash character."`);
+        expect(stripped).toMatchInlineSnapshot(`
+          "This is a test for use of the backslash character in Markdown.                 In this process, several lines are created using it.
+          1. This is a test document.                 Several lines new lines are created using the backslash character."
+        `);
     });
 
     it('should remove URLs in backticks', () => {
