49140d9cb6feb66c2877eb8d19ed10c86e2491c6
John Hannagan <weetbix_@hotmail.com>
2026-03-20 20:33:29 +1100

    Use rollup instead of ncc

---
+++ rollup.config.ts
@@ -0,0 +1,25 @@
+// See: https://rollupjs.org/introduction/
+
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+
+const config = {
+    input: 'src/index.ts',
+    output: {
+        esModule: true,
+        file: 'dist/index.js',
+        format: 'es',
+        sourcemap: true,
+        sourcemapExcludeSources: true,
+    },
+    plugins: [
+        typescript({outDir: 'dist'}),
+        nodeResolve({preferBuiltins: true}),
+        commonjs(),
+        json(),
+    ],
+};
+
+export default config;
